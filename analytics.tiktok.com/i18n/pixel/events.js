7c115caba332c7daf3c4399081eaf76922c0863f
Apple <apple@Apples-MacBook-Pro.local>
2025-11-11 01:55:02 +0530

    first commit

---
+++ analytics.tiktok.com/i18n/pixel/events.js
@@ -0,0 +1,423 @@
+window[window["TiktokAnalyticsObject"]]._env = {
+    "env": "external",
+    "key": ""
+};
+window[window["TiktokAnalyticsObject"]]._variation_id = 'default';
+window[window["TiktokAnalyticsObject"]]._vids = '74813232';
+window[window["TiktokAnalyticsObject"]]._cc = 'IN';
+window[window.TiktokAnalyticsObject]._li || (window[window.TiktokAnalyticsObject]._li = {}), window[window.TiktokAnalyticsObject]._li["CKA47TJC77U7REM9C0R0"] = "6c481aa0-be71-11f0-ad06-865e662b05a6";
+window[window["TiktokAnalyticsObject"]]._cde = 390;;
+if (!window[window["TiktokAnalyticsObject"]]._server_unique_id) window[window["TiktokAnalyticsObject"]]._server_unique_id = '6c482ad3-be71-11f0-ad06-865e662b05a6';
+window[window["TiktokAnalyticsObject"]]._plugins = {
+    "AdvancedMatching": true,
+    "AutoAdvancedMatching": true,
+    "AutoClick": true,
+    "AutoConfig": true,
+    "Callback": true,
+    "DiagnosticsConsole": true,
+    "EnableLPV": true,
+    "EnrichIpv6": true,
+    "EnrichIpv6V2": true,
+    "EventBuilder": true,
+    "EventBuilderRuleEngine": false,
+    "HistoryObserver": true,
+    "Identify": true,
+    "JSBridge": false,
+    "Metadata": true,
+    "Monitor": false,
+    "PageData": true,
+    "PerformanceInteraction": false,
+    "RuntimeMeasurement": true,
+    "Shopify": true,
+    "WebFL": false
+};
+window[window["TiktokAnalyticsObject"]]._csid_config = {
+    "enable": true
+};
+window[window["TiktokAnalyticsObject"]]._auto_config = {
+    "open_graph": ["audience"],
+    "microdata": ["audience"],
+    "json_ld": ["audience"],
+    "meta": null
+};
+! function(e, n, i, d, o, t) {
+    var u, a, M = c()._static_map || [{
+            id: "MTQwMDFhOTU1MA",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !1,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1MQ",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !1,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1Mg",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !0,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1Mw",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !0,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1NA",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !1,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1NQ",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !1,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1Ng",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !0,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1Nw",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !0,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1OA",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !1,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1OQ",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !1,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1MTA",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !0,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1MTE",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !0,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1MTI",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !1,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1MTM",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !1,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1MTQ",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !0,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1MTU",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !0,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !1
+            }
+        }, {
+            id: "MTQwMDFhOTU1MTY",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !1,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MTc",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !1,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MTg",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !0,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MTk",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !0,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MjA",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !1,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MjE",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !1,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MjI",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !0,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MjM",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !0,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !1,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MjQ",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !1,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MjU",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !1,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MjY",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !0,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1Mjc",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !0,
+                JSBridge: !1,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1Mjg",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !1,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1Mjk",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !1,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MzA",
+            map: {
+                AutoAdvancedMatching: !1,
+                Shopify: !0,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !0
+            }
+        }, {
+            id: "MTQwMDFhOTU1MzE",
+            map: {
+                AutoAdvancedMatching: !0,
+                Shopify: !0,
+                JSBridge: !0,
+                EventBuilderRuleEngine: !0,
+                RemoveUnusedCode: !0
+            }
+        }],
+        e = (c()._static_map = M, a = "https://analytics.tiktok.com/i18n/pixel/static/", null == (e = u = {
+            "info": {
+                "pixelCode": "CKA47TJC77U7REM9C0R0",
+                "name": "S2S Studio",
+                "status": 0,
+                "setupMode": 0,
+                "partner": "GoogleTagManagerClient",
+                "advertiserID": "7270847649208008705",
+                "is_onsite": false,
+                "firstPartyCookieEnabled": true
+            },
+            "plugins": {
+                "Shopify": false,
+                "AdvancedMatching": {
+                    "email": true,
+                    "phone_number": true,
+                    "first_name": true,
+                    "last_name": true,
+                    "city": true,
+                    "state": true,
+                    "country": true,
+                    "zip_code": true
+                },
+                "AutoAdvancedMatching": null,
+                "Callback": true,
+                "Identify": true,
+                "Monitor": true,
+                "PerformanceInteraction": true,
+                "WebFL": true,
+                "AutoConfig": {
+                    "form_rules": null,
+                    "vc_rules": {}
+                },
+                "PageData": {
+                    "performance": false,
+                    "interaction": true
+                },
+                "DiagnosticsConsole": true,
+                "CompetitorInsight": true,
+                "EventBuilder": true,
+                "EnrichIpv6": true,
+                "HistoryObserver": {
+                    "dynamic_web_pageview": true
+                },
+                "RuntimeMeasurement": true,
+                "JSBridge": true,
+                "EventBuilderRuleEngine": true,
+                "RemoveUnusedCode": true,
+                "EnableLPV": false,
+                "AutoConfigV2": false
+            },
+            "rules": []
+        }) || null == (n = e.info) ? void 0 : n.pixelCode);
+
+    function l() {
+        return window && window.TiktokAnalyticsObject || "ttq"
+    }
+
+    function c() {
+        return window && window[l()]
+    }
+
+    function g(e, n) {
+        n = c()[n];
+        return n && n[e] || {}
+    }
+    var r, v, n = c();
+    n || (n = [], window && (window[l()] = n)), Object.assign(u, {
+        options: g(e, "_o")
+    }), r = u, n._i || (n._i = {}), (v = r.info.pixelCode) && (n._i[v] || (n._i[v] = []), Object.assign(n._i[v], r), n._i[v]._load = +new Date), Object.assign(u.info, {
+        loadStart: g(e, "_t"),
+        loadEnd: g(e, "_i")._load,
+        loadId: n._li && n._li[e] || ""
+    }), null != (i = (d = n).instance) && null != (o = i.call(d, e)) && null != (t = o.setPixelInfo) && t.call(o, u.info), r = function(e, n, i) {
+        var t = 0 < arguments.length && void 0 !== e ? e : {},
+            u = 1 < arguments.length ? n : void 0,
+            e = 2 < arguments.length ? i : void 0,
+            n = function(e, n) {
+                for (var i = 0; i < e.length; i++)
+                    if (n.call(null, e[i], i)) return e[i]
+            }(M, function(e) {
+                for (var i = e.map, n = Object.keys(i), d = function(e) {
+                        var n;
+                        return "JSBridge" === e ? "external" !== (null == (n = c()._env) ? void 0 : n.env) === i[e] : !(!t[e] || !u[e]) === i[e]
+                    }, o = 0; o < n.length; o++)
+                    if (!d.call(null, n[o], o)) return !1;
+                return !0
+            });
+        return n ? "".concat(e, "main.").concat(n.id, ".js") : "".concat(e, "main.").concat(M[0].id, ".js")
+    }(n._plugins, u.plugins, a), v = e, (void 0 !== self.DedicatedWorkerGlobalScope ? self instanceof self.DedicatedWorkerGlobalScope : "DedicatedWorkerGlobalScope" === self.constructor.name) ? self.importScripts && self.importScripts(r) : ((i = document.createElement("script")).type = "text/javascript", i.async = !0, i.src = r, i.setAttribute("data-id", v), (r = document.getElementsByTagName("script")[0]) && r.parentNode && r.parentNode.insertBefore(i, r))
+}();
